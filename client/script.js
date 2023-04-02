70518fa249e6c817f0439778ca93855ae48a6d0e
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-02 13:29:16 +0200

    updated server url for script js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-tool.onrender.com', {
+    const response = await fetch('https://codex-ai-6hne.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
