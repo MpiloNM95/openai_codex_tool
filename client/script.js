83a5fb5d41bc3e294786e4702577b2adb43439e6
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-04 14:40:01 +0200

    added explalnation for code in both server and script js

--- client/script.js
+++ client/script.js
@@ -1,13 +1,13 @@
-import bot from './assets/bot.svg'
-import user from './assets/user.svg'
+import bot from './assets/bot.svg' // import bot svg
+import user from './assets/user.svg' // import user svg
 
-const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat_container')
+const form = document.querySelector('form') // form element
+const chatContainer = document.querySelector('#chat_container') // chat container element
 
-let loadInterval
+let loadInterval // to store the interval for the loading indicator
 
-function loader(element) {
-    element.textContent = ''
+function loader(element) { // loading indicator
+    element.textContent = '' // to clear the text content of the element
 
     loadInterval = setInterval(() => {
         // Update the text content of the loading indicator
@@ -17,18 +17,22 @@ function loader(element) {
         if (element.textContent === '....') {
             element.textContent = '';
         }
-    }, 300);
+    }, 300); // Repeat every 300ms
 }
 
-function typeText(element, text) {
-    let index = 0
-
-    let interval = setInterval(() => {
-        if (index < text.length) {
-            element.innerHTML += text.charAt(index)
-            index++
-        } else {
-            clearInterval(interval)
+ // typing text effect
+function typeText(element, text) { 
+    // to keep track of the index of the text
+    let index = 0 
+    // to type the text
+    // if the index is less than the length of the text, add the character at the index to the element, and increment the index, else clear the interval, i.e. stop the typing effect
+    // 20ms is the time interval between each character
+    let interval = setInterval(() => { 
+        if (index < text.length) { 
+            element.innerHTML += text.charAt(index) 
+            index++ 
+        } else { 
+            clearInterval(interval) 
         }
     }, 20)
 }
@@ -44,10 +48,14 @@ function generateUniqueId() {
     return `id-${timestamp}-${hexadecimalString}`;
 }
 
-function chatStripe(isAi, value, uniqueId) {
-    return (
+// function chatStripe(isAi, value, uniqueId) means the function takes three parameters
+// isAi is a boolean value to check if the chat stripe is from the bot or the user
+// value is the text content of the chat stripe
+// uniqueId is the unique ID of the message div of the bot
+function chatStripe(isAi, value, uniqueId) { 
+    return ( 
         `
-        <div class="wrapper ${isAi && 'ai'}">
+        <div class="wrapper ${isAi && 'ai'}"> 
             <div class="chat">
                 <div class="profile">
                     <img 
@@ -62,7 +70,11 @@ function chatStripe(isAi, value, uniqueId) {
     )
 }
 
-const handleSubmit = async (e) => {
+// handles the form submission
+// e is the event object
+// async function to handle the asynchronous nature of fetch
+// const data = new FormData(form) to get the form data
+const handleSubmit = async (e) => { 
     e.preventDefault()
 
     const data = new FormData(form)
@@ -85,7 +97,11 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
-
+    
+    // fetch request to the server
+    // method: POST means we are sending data to the server
+    // headers: Content-Type: application/json means we are sending JSON data
+    // body: JSON.stringify({ prompt: data.get('prompt') }) means we are sending the prompt value of the form
     const response = await fetch('https://codex-the-dev-tool.onrender.com', {
         method: 'POST',
         headers: {
@@ -96,6 +112,14 @@ const handleSubmit = async (e) => {
         })
     })
 
+    // clearInterval(loadInterval) to clear the interval
+    // messageDiv.innerHTML = " " to clear the loading indicator
+    // trims any trailing spaces/'\n' 
+    // typeText(messageDiv, parsedData) means to type the text
+    // else alert the error
+    // to focus scroll to the bottom
+    // to clear the textarea input
+    // messageDiv.innerHTML = "Something went wrong" means to display the error message
     clearInterval(loadInterval)
     messageDiv.innerHTML = " "
 
@@ -112,6 +136,8 @@ const handleSubmit = async (e) => {
     }
 }
 
+// form.addEventListener('submit', handleSubmit) means to call the handleSubmit function when the form is submitted
+// form.addEventListener('keyup', (e) => { if (e.keyCode === 13) { handleSubmit(e); } }) means to call the handleSubmit function when the enter key is pressed
 form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
--- server/server.js
+++ server/server.js
@@ -1,26 +1,43 @@
+// import express from 'express' measn that we are importing the express library
 import express from 'express'
+// import dotenv from 'dotenv' means that we are importing the dotenv library
 import * as dotenv from 'dotenv'
+// import cors from 'cors' means that we are importing the cors library
 import cors from 'cors'
+// import { Configuration, OpenAIApi } from 'openai' means that we are importing the Configuration and OpenAIApi classes from the openai library
 import { Configuration, OpenAIApi } from 'openai'
 
+// dotenv.config() loads the environment variables from the .env file
 dotenv.config()
 
+// const configuration = new Configuration means that we are creating a new instance of the Configuration class
+// apiKey: process.env.OPENAI_API_KEY means that we are passing the OPENAI_API_KEY environment variable as the apiKey
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// const openai = new OpenAIApi(configuration) means that we are creating a new instance of the OpenAIApi class
 const openai = new OpenAIApi(configuration);
 
+// const app = express() means that we are creating a new instance of the express class
+// app.use(cors({origin:"*"})) means that we are using the cors middleware
+// app.use(express.json()) means that we are using the express.json() middleware
 const app = express()
 app.use(cors({origin:"*"}));
 app.use(express.json())
 
+// app.get('/', async (req, res) => means that we are creating a new route
+// res.status(200).send({ means that we are sending a response with a status code of 200
+// message: 'Hello from CodeX!' means that we are sending a message
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from CodeX!'
   })
 })
 
+// app.post('/', async (req, res) => means that we are creating a new route
+// try means that we are trying to run the code inside the try block
+// const prompt = req.body.prompt; means that we are getting the prompt value from the request body
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
@@ -35,14 +52,21 @@ app.post('/', async (req, res) => {
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
+    // res.status(200).send({ means that we are sending a response with a status code of 200
+    // bot: response.data.choices[0].text means that we are sending the bot's response
     res.status(200).send({
       bot: response.data.choices[0].text
     });
 
+    // catch means that we are catching any errors that occur in the try block
+    // console.error(error) means that we are logging the error to the console
+    // res.status(500).send(error || 'Something went wrong'); means that we are sending a response with a status code of 500
+    // error || 'Something went wrong' means that we are sending the error or a message if there is no error
   } catch (error) {
     console.error(error)
     res.status(500).send(error || 'Something went wrong');
   }
 })
 
+// app.listen(5050, () => console.log('AI server started on http://localhost:5050')) means that we are listening on port 5050
 app.listen(5050, () => console.log('AI server started on http://localhost:5050'))
